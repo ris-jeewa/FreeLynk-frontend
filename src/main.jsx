b08f583d3385d53bcde55b61c9da0322db171b0c
risni <125817506+ris-jeewa@users.noreply.github.com>
2025-04-25 21:38:00 +0530

    added okta auth0

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Login from './Login/Login.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Login from "./Login/Login.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById("root"));
+
+const auth0Domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+const scope = "openid profile delete:resources write:advices access:admin";
+
+root.render(
+  <BrowserRouter>
+    <Auth0Provider
+      domain={auth0Domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </BrowserRouter>
+);
