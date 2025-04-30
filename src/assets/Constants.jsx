e0aa01688ec4813a634988047e6c6040849853b3
risni <125817506+ris-jeewa@users.noreply.github.com>
2025-04-30 19:47:42 +0530

    added profile details of hero img to assets

---
+++ src/assets/Constants.jsx
@@ -0,0 +1,8 @@
+export const profiles = [
+    { name: 'Adam Crawford', image: '/avatars/hero-img-4.jpeg', rotation: 0, distance: 160 },
+    { name: 'Christina Jones', image: '/avatars/hero-img-2.jpeg', rotation: 60, distance: 160 },
+    { name: 'James Warner', image: '/avatars/hero-img-3.jpeg', rotation: 120, distance: 160 },
+    { name: 'Sophia Garner', image: '/avatars/hero-img-1.jpeg', rotation: 180, distance: 160 },
+    { name: 'Kevin Kapoor', image: '/avatars/hero-img-6.jpeg', rotation: 240, distance: 160 },
+    { name: 'Megan Trainer', image: '/avatars/hero-img-7.jpeg', rotation: 300, distance: 160 },
+  ];
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { gsap } from 'gsap'
 import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa'
+import { profiles } from '../assets/Constants';
 
-const NetworkCircle = ({ image, name, position, rotation, distance }) => {
+const NetworkCircle = ({ image, name, rotation, distance }) => {
   const imageRef = useRef(null);
 
   useEffect(() => {
@@ -82,15 +83,7 @@ const TypewriterText = ({ text }) => {
 };
 
 export const Home = () => {
-  const profiles = [
-    { name: 'Adam Crawford', image: '/avatars/hero-img-4.jpeg', rotation: 0, distance: 160 },
-    { name: 'Christina Jones', image: '/avatars/hero-img-2.jpeg', rotation: 60, distance: 160 },
-    { name: 'James Warner', image: '/avatars/hero-img-3.jpeg', rotation: 120, distance: 160 },
-    { name: 'Sophia Garner', image: '/avatars/hero-img-1.jpeg', rotation: 180, distance: 160 },
-    { name: 'Kevin Kapoor', image: '/avatars/hero-img-6.jpeg', rotation: 240, distance: 160 },
-    { name: 'Megan Trainer', image: '/avatars/hero-img-7.jpeg', rotation: 300, distance: 160 },
-  ];
-
+  
   return (
     <div className="min-h-screen bg-[#1A1A1A]">
       <div className="px-8">
