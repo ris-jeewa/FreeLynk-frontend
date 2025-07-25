94eed7661b638bd398c627efd5f1e75bd0b3980c
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-07-26 01:06:53 +0530

    added hmr

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,13 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+    hmr: {
+      overlay: true,
+    },
+  },
 })
