7682a6e79bcb35aea7ef9de8761c7c98a5161b09
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-09-14 23:10:07 +0530

    import asgardeopro from "@asgardeo/auth-react";

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,14 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import { AsgardeoProvider } from "@asgardeo/react"
 
 const root = createRoot(document.getElementById("root"));
 
-const auth0Domain = import.meta.env.VITE_AUTH0_DOMAIN;
-const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
-const scope = "openid profile delete:resources write:advices access:admin";
+const clientId = import.meta.env.VITE_ASGARDEO_CLIENT_ID;
+const baseUrl = import.meta.env.VITE_ASGARDEO_BASE_URL;
 
 // const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 const googleClientId = "288242518278-fn2dg6830vf4sstgd7qri10501ve7i20.apps.googleusercontent.com";
@@ -20,8 +16,8 @@ const googleClientId = "288242518278-fn2dg6830vf4sstgd7qri10501ve7i20.apps.googl
 root.render(
   <BrowserRouter>
       <AsgardeoProvider
-        clientId=""
-        baseUrl=""
+        clientId={clientId}
+        baseUrl={baseUrl}
       >
         <App />
         <Toaster position="bottom-right" />
