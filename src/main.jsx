10b8d422f41f8e5bc2bcfb7a1dfef2f6e91f9372
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-09-17 23:57:54 +0530

    refactor authentication setup by replacing AsgardeoProvider with AuthProvider and updating configuration parameters

--- src/main.jsx
+++ src/main.jsx
@@ -3,24 +3,24 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { AsgardeoProvider } from "@asgardeo/react"
+import { AuthProvider } from "@asgardeo/auth-react";
 
 const root = createRoot(document.getElementById("root"));
-
-const clientId = import.meta.env.VITE_ASGARDEO_CLIENT_ID;
-const baseUrl = import.meta.env.VITE_ASGARDEO_BASE_URL;
-
-// const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-const googleClientId = "288242518278-fn2dg6830vf4sstgd7qri10501ve7i20.apps.googleusercontent.com";
+const config = {
+  signInRedirectURL: "http://localhost:5173",
+  signOutRedirectURL: "http://localhost:5173",
+  clientID: "yC7UNGj7suGPk1jylBCAwZ5LVuwa",
+  baseUrl: "https://localhost:9443",
+  // scope: [ "openid","profile" ]
+};
 
 root.render(
   <BrowserRouter>
-      <AsgardeoProvider
-        clientId={clientId}
-        baseUrl={baseUrl}
+      <AuthProvider
+        config={config}
       >
         <App />
         <Toaster position="bottom-right" />
-      </AsgardeoProvider>
+      </AuthProvider>
   </BrowserRouter>
 );
