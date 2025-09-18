45776c5a72701ebe5af07386bb466a5f8e8bd3c9
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-09-18 23:23:48 +0530

    update Asgardeo configuration to include additional scopes and improve code formatting

--- src/main.jsx
+++ src/main.jsx
@@ -6,21 +6,21 @@ import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "@asgardeo/auth-react";
 
 const root = createRoot(document.getElementById("root"));
+
+// Asgardeo configuration
 const config = {
   signInRedirectURL: "http://localhost:5173",
   signOutRedirectURL: "http://localhost:5173",
   clientID: "yC7UNGj7suGPk1jylBCAwZ5LVuwa",
   baseUrl: "https://localhost:9443",
-  // scope: [ "openid","profile" ]
+  scope: ["openid", "profile", "email", "roles"]
 };
 
 root.render(
   <BrowserRouter>
-      <AuthProvider
-        config={config}
-      >
-        <App />
-        <Toaster position="bottom-right" />
-      </AuthProvider>
+    <AuthProvider config={config}>
+      <App />
+      <Toaster position="bottom-right" />
+    </AuthProvider>
   </BrowserRouter>
 );
