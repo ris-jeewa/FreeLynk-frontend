22fa2eeac9fa41da9125840959ac2adb2e0e7547
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-09-24 21:51:07 +0530

    removed authentication configs

--- src/main.jsx
+++ src/main.jsx
@@ -3,24 +3,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { AuthProvider } from "@asgardeo/auth-react";
 
 const root = createRoot(document.getElementById("root"));
 
-// Asgardeo configuration
-const config = {
-  signInRedirectURL: "http://localhost:5173",
-  signOutRedirectURL: "http://localhost:5173",
-  clientID: "yC7UNGj7suGPk1jylBCAwZ5LVuwa",
-  baseUrl: "https://localhost:9443",
-  scope: ["openid", "profile", "email", "roles"]
-};
 
 root.render(
   <BrowserRouter>
-    <AuthProvider config={config}>
       <App />
       <Toaster position="bottom-right" />
-    </AuthProvider>
   </BrowserRouter>
 );
