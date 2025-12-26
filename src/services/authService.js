0800bfa73c68a450c0f28a0db08a9500ce717e38
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-12-26 17:43:09 +0530

    deleted previous auth files

--- src/services/authService.js
+++
@@ -1,9 +0,0 @@
-import api from '../api/apiClient'; 
-
-export const register = async (userData) => {
-  return api.post('/api/auth/register', userData);
-};
-
-export const login = async (userData) => {
-  return api.post('/api/auth/login', userData);
-};
