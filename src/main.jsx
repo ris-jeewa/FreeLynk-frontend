8f277c71726063c18eb1c8d6d056d606a7bd04f6
ris-jeewa <dheerasekararj.21@uom.lk>
2026-04-06 23:55:05 +0530

    Integrate Auth0Provider in main.jsx for authentication, enhancing app security and user management.

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,26 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = createRoot(document.getElementById("root"));
 
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+
 root.render(
-  <BrowserRouter>
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{
+      audience,
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <BrowserRouter>
       <App />
       <Toaster position="bottom-right" />
-  </BrowserRouter>
+    </BrowserRouter>
+  </Auth0Provider>
 );
